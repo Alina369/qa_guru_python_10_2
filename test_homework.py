8f989eab6642a3763e9422cf8c3c24faeb1d5764
Alina <lav0903@mail.ru>
2024-01-17 21:29:32 +0300

    add homework wrong test

--- test_homework.py
+++ test_homework.py
@@ -6,3 +6,8 @@ def test_find_selene():
     browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_find_wrong():
+    browser.open("https://google.com")
+    browser.element('[name="q"]').should(be.blank).type('fghfghdjkghkdujh').press_enter()
+    browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
